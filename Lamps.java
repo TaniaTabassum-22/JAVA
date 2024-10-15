2313b80f7bd4e54f031c47ba9cb6e35e9d894375
TaniaTabassum-22 <taniatabassum952@gmail.com>
2024-10-15 11:57:22 +0530

    class and object

---
+++ Lamps.java
@@ -0,0 +1,24 @@
+public class Lamps {
+    
+    String brand;
+    String type;
+    int watts; 
+    String color; 
+    boolean isSmartLamp;
+
+    public static void turnOn() {
+
+    }
+
+    public static void turnOff() {
+       
+    }
+
+    public static void dimLight(int level) {
+        
+    }
+
+    public static void changeColor(String color) {
+        
+    }
+}
