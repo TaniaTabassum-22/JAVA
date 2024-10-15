269225672ae6a440e35f2291f33f74f23b401427
TaniaTabassum-22 <taniatabassum952@gmail.com>
2024-10-15 12:01:27 +0530

    variables

---
+++ VariablesDemo.java
@@ -0,0 +1,60 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        // Integer variables
+        int age = 25;
+        int maxScore = 100;
+        int distance = 1500;
+        int population = 120000;
+        int temperature = 35;
+        int depth = -200;
+        int rank = 3;
+        int height = 180;
+
+        // Long variables
+        long worldPopulation = 7800000000L;
+        long nationalDebt = 23000000000L;
+        long distanceToMoon = 384400000L;
+        long lightSpeed = 299792458L;
+        long bankBalance = 1500000000L;
+        long fileSize = 5000000000L;
+        long mountainHeight = 8848000L;
+        long spaceMissionYear = 2035;
+
+        // Displaying int variables
+        System.out.println("Integer variables:");
+        System.out.println("age: " + age);
+        age = 30;
+        System.out.println("Updated age: " + age);
+        System.out.println("maxScore: " + maxScore);
+        System.out.println("distance: " + distance);
+        distance = 2000;
+        System.out.println("Updated distance: " + distance);
+        System.out.println("population: " + population);
+        System.out.println("temperature: " + temperature);
+        temperature = 40;
+        System.out.println("Updated temperature: " + temperature);
+        System.out.println("depth: " + depth);
+        System.out.println("rank: " + rank);
+        rank = 1;
+        System.out.println("Updated rank: " + rank);
+        System.out.println("height: " + height);
+
+        // Displaying long variables
+        System.out.println("\nLong variables:");
+        System.out.println("worldPopulation: " + worldPopulation);
+        System.out.println("nationalDebt: " + nationalDebt);
+        nationalDebt = 25000000000L;
+        System.out.println("Updated nationalDebt: " + nationalDebt);
+        System.out.println("distanceToMoon: " + distanceToMoon);
+        System.out.println("lightSpeed: " + lightSpeed);
+        System.out.println("bankBalance: " + bankBalance);
+        bankBalance = 1700000000L;
+        System.out.println("Updated bankBalance: " + bankBalance);
+        System.out.println("fileSize: " + fileSize);
+        System.out.println("mountainHeight: " + mountainHeight);
+        System.out.println("spaceMissionYear: " + spaceMissionYear);
+        spaceMissionYear = 2040;
+        System.out.println("Updated spaceMissionYear: " + spaceMissionYear);
+    }
+}
