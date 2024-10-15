a9a43ccf3b5867d4793fe521fcb7dcf928cf4fc4
TaniaTabassum-22 <taniatabassum952@gmail.com>
2024-10-15 11:59:16 +0530

    class and object

---
+++ LampDetails.java
@@ -0,0 +1,146 @@
+public class LampDetails {
+    public static void main(String[] args) {
+        
+        Lamps lamp1 = new Lamps();
+        lamp1.brand = "Philips";
+        lamp1.type = "Table lamp";
+        lamp1.watts = 60;
+        lamp1.color = "White";
+        lamp1.isSmartLamp = true;
+
+        Lamps lamp2 = new Lamps();
+        lamp2.brand = "Ikea";
+        lamp2.type = "Floor lamp";
+        lamp2.watts = 100;
+        lamp2.color = "Black";
+        lamp2.isSmartLamp = false;
+
+        Lamps lamp3 = new Lamps();
+        lamp3.brand = "Mi";
+        lamp3.type = "Smart Desk lamp";
+        lamp3.watts = 12;
+        lamp3.color = "Silver";
+        lamp3.isSmartLamp = true;
+
+        Lamps lamp4 = new Lamps();
+        lamp4.brand = "Syska";
+        lamp4.type = "Night lamp";
+        lamp4.watts = 5;
+        lamp4.color = "Blue";
+        lamp4.isSmartLamp = false;
+
+        Lamps lamp5 = new Lamps();
+        lamp5.brand = "Wipro";
+        lamp5.type = "Ceiling lamp";
+        lamp5.watts = 40;
+        lamp5.color = "Grey";
+        lamp5.isSmartLamp = true;
+
+        Lamps lamp6 = new Lamps();
+        lamp6.brand = "Havells";
+        lamp6.type = "Wall lamp";
+        lamp6.watts = 20;
+        lamp6.color = "Gold";
+        lamp6.isSmartLamp = false;
+
+        Lamps lamp7 = new Lamps();
+        lamp7.brand = "Panasonic";
+        lamp7.type = "Outdoor lamp";
+        lamp7.watts = 75;
+        lamp7.color = "Brown";
+        lamp7.isSmartLamp = true;
+
+        Lamps lamp8 = new Lamps();
+        lamp8.brand = "Crompton";
+        lamp8.type = "Study lamp";
+        lamp8.watts = 25;
+        lamp8.color = "Pink";
+        lamp8.isSmartLamp = false;
+
+        Lamps lamp9 = new Lamps();
+        lamp9.brand = "Orient";
+        lamp9.type = "Chandelier lamp";
+        lamp9.watts = 60;
+        lamp9.color = "Crystal";
+        lamp9.isSmartLamp = false;
+
+        Lamps lamp10 = new Lamps();
+        lamp10.brand = "Syska";
+        lamp10.type = "LED lamp";
+        lamp10.watts = 18;
+        lamp10.color = "Green";
+        lamp10.isSmartLamp = true;
+        
+        
+        System.out.println("Brand: " + lamp1.brand);
+        System.out.println("Type: " + lamp1.type);
+        System.out.println("Watts: " + lamp1.watts + "W");
+        System.out.println("Color: " + lamp1.color);
+        System.out.println("Smart Lamp: " + lamp1.isSmartLamp);
+        System.out.println("-----------------------------");
+
+        System.out.println("Brand: " + lamp2.brand);
+        System.out.println("Type: " + lamp2.type);
+        System.out.println("Watts: " + lamp2.watts + "W");
+        System.out.println("Color: " + lamp2.color);
+        System.out.println("Smart Lamp: " + lamp2.isSmartLamp);
+        System.out.println("-----------------------------");
+
+        System.out.println("Brand: " + lamp3.brand);
+        System.out.println("Type: " + lamp3.type);
+        System.out.println("Watts: " + lamp3.watts + "W");
+        System.out.println("Color: " + lamp3.color);
+        System.out.println("Smart Lamp: " + lamp3.isSmartLamp);
+        System.out.println("-----------------------------");
+
+        System.out.println("Brand: " + lamp4.brand);
+        System.out.println("Type: " + lamp4.type);
+        System.out.println("Watts: " + lamp4.watts + "W");
+        System.out.println("Color: " + lamp4.color);
+        System.out.println("Smart Lamp: " + lamp4.isSmartLamp);
+        System.out.println("-----------------------------");
+
+        System.out.println("Brand: " + lamp5.brand);
+        System.out.println("Type: " + lamp5.type);
+        System.out.println("Watts: " + lamp5.watts + "W");
+        System.out.println("Color: " + lamp5.color);
+        System.out.println("Smart Lamp: " + lamp5.isSmartLamp);
+        System.out.println("-----------------------------");
+
+        System.out.println("Brand: " + lamp6.brand);
+        System.out.println("Type: " + lamp6.type);
+        System.out.println("Watts: " + lamp6.watts + "W");
+        System.out.println("Color: " + lamp6.color);
+        System.out.println("Smart Lamp: " + lamp6.isSmartLamp);
+        System.out.println("-----------------------------");
+
+        System.out.println("Brand: " + lamp7.brand);
+        System.out.println("Type: " + lamp7.type);
+        System.out.println("Watts: " + lamp7.watts + "W");
+        System.out.println("Color: " + lamp7.color);
+        System.out.println("Smart Lamp: " + lamp7.isSmartLamp);
+        System.out.println("-----------------------------");
+
+        System.out.println("Brand: " + lamp8.brand);
+        System.out.println("Type: " + lamp8.type);
+        System.out.println("Watts: " + lamp8.watts + "W");
+        System.out.println("Color: " + lamp8.color);
+        System.out.println("Smart Lamp: " + lamp8.isSmartLamp);
+        System.out.println("-----------------------------");
+
+        System.out.println("Brand: " + lamp9.brand);
+        System.out.println("Type: " + lamp9.type);
+        System.out.println("Watts: " + lamp9.watts + "W");
+        System.out.println("Color: " + lamp9.color);
+        System.out.println("Smart Lamp: " + lamp9.isSmartLamp);
+        System.out.println("-----------------------------");
+        
+        System.out.println("Brand: " + lamp10.brand);
+        System.out.println("Type: " + lamp10.type);
+        System.out.println("Watts: " + lamp10.watts + "W");
+        System.out.println("Color: " + lamp10.color);
+        System.out.println("Smart Lamp: " + lamp10.isSmartLamp);
+        System.out.println("-----------------------------");
+
+    }
+}
